009a2566b04cac893064d839888bd4404d07b0d8
Komissarov Pavel <pavelkomiss@gmail.com>
2025-05-31 12:19:53 +0300

    Initial commit: Create Gradle project

---
+++ src/main/java/org/example/Greeting.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Greeting {
+    public String greeting(){
+        return "Hello MP-10 done";
+    }
+}
---
+++ src/test/java/org/example/GreetingTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+    @Test
+    void shouldGreeting(){
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals(result, "Hello MP-10 done");
+    }
+}
